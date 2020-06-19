ae9017360c3c93220c49cc7c62b4e0180c4b9439
jiikko <n905i.1214@gmail.com>
2020-06-19 21:54:25 +0900

    fix

--- lib/onetime_script_cop.rb
+++ lib/onetime_script_cop.rb
@@ -1,6 +1,13 @@
 require "onetime_script_cop/version"
+require "rubocop"
 
 module OnetimeScriptCop
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Cop < RuboCop::Cop::Cop
+    def on_str(node)
+      binding.pry
+      add_offense(node) if node.source.include?("")
+    end
+  end
 end
